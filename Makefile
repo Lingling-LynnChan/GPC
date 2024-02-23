9fddaf026fd9f6f19545e57d41904689eb989fa2
msi_win_wsl <linglingright@outlook.com>
2024-02-23 17:08:49 +0800

    update 2024-02-23 17:08:49

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-TOP_MODULE  := Multiplier32
+TOP_MODULE  := CSA4T2
 
 SIM_FILE    := main.cpp
 
--- csrc/main.cpp
+++ csrc/main.cpp
@@ -1,12 +1,12 @@
 #include <cstdint>
 #include <iostream>
 #include <random>
-#define NAME VMultiplier32
+#define NAME VCSA4T2
 #ifndef _FAKE_VSCODE_LINT
 #include <verilated.h>
 #include <verilated_vcd_c.h>
 
-#include "VMultiplier32.h"  // 替换为顶层模块的文件名
+#include "VCSA4T2.h"  // 替换为顶层模块的文件名
 #else
 // 欺骗代码提示，假装存在这些类
 typedef volatile uint64_t vluint64_t;
@@ -26,11 +26,14 @@ class VerilatedVcdC {
   void dump(vluint64_t i) {}
 };
 struct NAME {
+  uint32_t cin;
   uint32_t in1;
   uint32_t in2;
-  uint32_t cin;
+  uint32_t in3;
+  uint32_t in4;
   uint32_t out;
   uint32_t cout;
+  uint32_t carry;
   void eval() {}
   void final() {}
   void trace(VerilatedVcdC *vcd, int i) {}
@@ -52,37 +55,54 @@ int main(int argc, char **argv) {
   vcd->open("build/trace.vcd");
   // 仿真开始
   std::cout << "====================sim start===========================\n";
+#ifdef Multiplier32
   int32_t errnum = 0;  // 错误次数
-  if (true) {
-    // 随机数
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<uint32_t> distrib(0, UINT32_MAX);  // 范围
-    int n = 1;  // 仿真次数
-    uint32_t hf[2];
-    uint64_t max = UINT32_MAX;
-    uint32_t min = max - 1000;
-    while (n--) {
-      for (uint64_t i = min; i <= max; i++) {
-        for (uint64_t j = i; j <= max; j++) {
-          // hf[0] = distrib(gen);
-          hf[0] = i;
-          top->in1 = hf[0];  // 生成in1
-          // hf[1] = distrib(gen);
-          hf[1] = j;
-          top->in2 = hf[1];  // 生成in2
-          top->eval();
-          uint64_t ans = (uint64_t)hf[0] * hf[1];  // 标准结果
-          if (top->out != ans) {  // 比较仿真结果是否正确
-            errnum++;
-            printf("UNSG: %u * %u = %lu\n", hf[0], hf[1], top->out);
-            printf("ERROR ANSWER: %lu\n", ans);
-          }
+  // 随机数
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<uint32_t> distrib(0, UINT32_MAX);  // 范围
+  int n = 1;  // 仿真次数
+  uint32_t hf[2];
+  uint64_t max = UINT32_MAX;
+  uint32_t min = max - 1000;
+  while (n--) {
+    for (uint64_t i = min; i <= max; i++) {
+      for (uint64_t j = i; j <= max; j++) {
+        // hf[0] = distrib(gen);
+        hf[0] = i;
+        top->in1 = hf[0];  // 生成in1
+        // hf[1] = distrib(gen);
+        hf[1] = j;
+        top->in2 = hf[1];  // 生成in2
+        top->eval();
+        uint64_t ans = (uint64_t)hf[0] * hf[1];  // 标准结果
+        if (top->out != ans) {  // 比较仿真结果是否正确
+          errnum++;
+          printf("UNSG: %u * %u = %lu\n", hf[0], hf[1], top->out);
+          printf("ERROR ANSWER: %lu\n", ans);
         }
       }
     }
-    printf("HAS %d ERROR(s)\n", errnum);
   }
+  printf("HAS %d ERROR(s)\n", errnum);
+#else
+  uint64_t cin = 0;
+  uint64_t ins[4] = {10, 10, 10, 10};
+  top->in1 = ins[0];
+  top->in2 = ins[1];
+  top->in3 = ins[2];
+  top->in4 = ins[3];
+  top->cin = cin;
+  top->eval();
+  uint64_t out = top->out;
+  uint64_t cout = top->cout;
+  uint64_t carry = top->carry;
+  uint64_t sum = (cout << 1) + out + carry;
+  uint64_t ans = ins[0] + ins[1] + ins[2] + ins[3] + cin;
+  printf("ANSWER IS %s: (%lu)|(%lu)\n", sum == ans ? "TRUE" : "FALSE", sum,
+         ans);
+
+#endif
   std::cout << "====================sim end=============================\n";
   // 释放资源
   vcd->close();
