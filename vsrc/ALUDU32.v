5b3f70b88bd2174081f31dfc5da2d74ba9968951
msi_win_wsl <linglingright@outlook.com>
2024-03-12 22:26:08 +0800

    update 2024-03-12 22:26:08

--- vsrc/ALUDU32.v
+++ vsrc/ALUDU32.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module ALDU (  //Arithmetic Logic Decode Unit: 把指令译为算数逻辑操作码
+module ALDU32 (  //Arithmetic Logic Decode Unit: 把指令译为算数逻辑操作码
     input [3:0] funct3,
     input [7:0] funct7,
     input funct7_en,  //imm指令的funct7是无效的
