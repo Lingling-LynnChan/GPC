12d0812e30ad5337fdfbfd061b846679d6560a27
msi_win_wsl <linglingright@outlook.com>
2024-03-15 01:10:04 +0800

    update 2024-03-15 01:10:04

--- vsrc/GPC32.v
+++ vsrc/GPC32.v
@@ -28,6 +28,7 @@ module GPC32 #(  //Gwen Processor Core: Single Cycle Processor 32 bit
   wire [WIDTH-1:0] sin2;
   assign sin1 = regsio[1] ? regs_dina : addrs1;
   assign sin2 = regsio[2] ? regs_dinb : addrs2;
+  // assign regs_dinw
   //模块连线
   Regs #(
       .WIDTH     (WIDTH),
