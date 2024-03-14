deaa0b694c4b392df013d1c85476bfaec230f829
msi_win_wsl <linglingright@outlook.com>
2024-03-14 14:21:52 +0800

    update 2024-03-14 14:21:52

--- vsrc/ALU32.v
+++ vsrc/ALU32.v
@@ -7,18 +7,5 @@ module ALU32 (
     input  [31:0] in2,
     output [31:0] out
 );
-  /*
-alu_code
-add               0 加
-sub               1 减
-xor               2 异或
-or                3 或
-and               4 与
-sll               5 左移
-srl               6 右移（逻辑）
-sra               7 右移（算数）
-slt               8 小于（有符号）
-sltu              9 小于（无符号）
-*/
 
 endmodule
