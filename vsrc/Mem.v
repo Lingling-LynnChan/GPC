a97f66ef13fd059d4ba6bb80cd4b30bd79881af6
msi_win_wsl <linglingright@outlook.com>
2024-03-14 23:12:26 +0800

    update 2024-03-14 23:12:26

---
+++ vsrc/Mem.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+
+module Mem #(
+    WIDTH = 32
+) (
+    input clk,
+    input rst,
+    input wen1,
+    input [WIDTH-1:0] addrw1,
+    input [WIDTH-1:0] dataw1,
+    input wen2,
+    input [WIDTH-1:0] addrw2,
+    input [WIDTH-1:0] dataw2,
+    input rena,
+    input [WIDTH-1:0] addra,
+    input renb,
+    input [WIDTH-1:0] addrb,
+    output [WIDTH-1:0] outa,
+    output [WIDTH-1:0] outb
+);
+  //TODO: RAM IO
+  assign outa = 0;
+  assign outb = 0;
+endmodule
